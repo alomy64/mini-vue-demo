d692343f97b43523026446f52dd41105699446ce
alomy64 <alomy64@163.com>
2022-04-05 11:38:19 +0800

    chore: Format document

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,43 +1,43 @@
 class ReactiveEffect {
-  private _fn: any
+  private _fn: any;
   constructor(fn) {
-    this._fn = fn
+    this._fn = fn;
   }
   run() {
-    activeEffect = this
-    return this._fn()
+    activeEffect = this;
+    return this._fn();
   }
 }
 
-const targetMap = new Map()
+const targetMap = new Map();
 // 依赖收集
 export function track(target, key) {
-  let depsMap = targetMap.get(target)
+  let depsMap = targetMap.get(target);
   if (!depsMap) {
-    depsMap = new Map()
-    targetMap.set(target, depsMap)
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
   }
-  let dep = depsMap.get(key)
+  let dep = depsMap.get(key);
   if (!dep) {
-    dep = new Set()
-    depsMap.set(key, dep)
+    dep = new Set();
+    depsMap.set(key, dep);
   }
-  dep.add(activeEffect)
+  dep.add(activeEffect);
 }
 // 触发依赖
 export function trigger(target, key) {
-  let depsMap = targetMap.get(target)
-  let dep = depsMap.get(key)
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
 
   for (const effect of dep) {
-    effect.run()
+    effect.run();
   }
 }
 
-let activeEffect
+let activeEffect;
 export function effect(fn) {
-  const _effect = new ReactiveEffect(fn)
-  _effect.run()
+  const _effect = new ReactiveEffect(fn);
+  _effect.run();
 
-  return _effect.run.bind(_effect)
+  return _effect.run.bind(_effect);
 }
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -3,16 +3,16 @@ import { track, trigger } from "./effect";
 export function reactive(raw) {
   return new Proxy(raw, {
     get(target, key) {
-      const res = Reflect.get(target, key)
+      const res = Reflect.get(target, key);
 
-      track(target, key) 
-      return res
+      track(target, key);
+      return res;
     },
     set(target, key, value) {
-      const res = Reflect.set(target, key, value)
+      const res = Reflect.set(target, key, value);
 
-      trigger(target, key)
-      return res
-    }
-  })
+      trigger(target, key);
+      return res;
+    },
+  });
 }
